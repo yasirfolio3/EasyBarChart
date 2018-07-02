3db9c6d790de62dddc82e09f2a58cb61d0e8a00f
Yasir Ali <>
2018-07-02 14:59:32 +0500

    Core implementations

---
+++ Classes/CustomFlowLayout.swift
@@ -0,0 +1,29 @@
+//
+//  CustomFlowLayout.swift
+//  EasyBarChart
+//
+//  Created by Yasir Ali on 26/06/2018.
+//  Copyright © 2018 Yasir Ali. All rights reserved.
+//
+
+import UIKit
+
+class CustomFlowLayout: UICollectionViewFlowLayout {
+    
+    override init() {
+        super.init()
+    }
+    
+    override func prepare() {
+        super.prepare()
+        
+        self.sectionInset = UIEdgeInsets(top: 0, left: 0, bottom: 0, right: 0)
+        self.minimumInteritemSpacing = 0
+        self.minimumLineSpacing = 0
+        self.scrollDirection = .horizontal
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+    }
+}
--- Example/EasyBarChart/ViewController.swift
+++ Example/EasyBarChart/ViewController.swift
@@ -7,18 +7,70 @@
 //
 
 import UIKit
+import EasyBarChart
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController,UICollectionViewDelegate,UICollectionViewDataSource {
+    
+    @IBOutlet weak var collectionView: UICollectionView!
+    var layout: CustomFlowLayout!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
-
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        self.updateFlowLayout()
+    }
+    
+    override func viewWillTransition(to size: CGSize, with coordinator: UIViewControllerTransitionCoordinator) {
+        super.viewWillTransition(to: size, with: coordinator)
+        self.updateFlowLayout()
+    }
+    
+    @objc func updateFlowLayout() {
+        
+        if(layout == nil){
+            layout = CustomFlowLayout.init()
+        }
+        
+        let width: CGFloat = self.collectionView.frame.size.width
+        let height: CGFloat = self.collectionView.frame.size.height
+        let cellWidth: CGFloat = (width/5 - 5)
+        let cellHeight: CGFloat = height - 5
+        
+        layout.itemSize = CGSize(width: cellWidth, height: cellHeight)
+        collectionView!.collectionViewLayout = layout
+        collectionView.isPagingEnabled = false
+        collectionView.reloadData()
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 20
+    }
+    
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return 1
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell : BarCollectionViewCell = collectionView.dequeueReusableCell(withReuseIdentifier: "BarCollectionViewCell", for: indexPath) as! BarCollectionViewCell
+        
+        cell.showArrow = (indexPath.row % 2 == 0)
+        cell.barValue = 50
+        cell.barSize = 50
+        cell.backgroundImage = UIImage(named: "paper")
+        cell.separatorColor = UIColor.darkGray
+        cell.categoryImage = UIImage(named: "fruit")
+        cell.categoryName = "NUTRITION"
+        cell.themeColor = UIColor.purple
+        return cell
+    }
+    
 }
+
 
